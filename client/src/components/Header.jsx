a904fac65056afa5d802e2db0f5b000fbf143c81
CassioUrtiga <henrique.lucas521@gmail.com>
2023-03-21 12:57:59 -0300

    Melhorias na responsividade

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,26 +1,27 @@
 import { Link } from 'react-router-dom';
+import logo from "../assets/logo.png";
 
 export function Header() {
 	return (
-		<header className="flex w-full justify-between bg-slate-900 px-14 py-5 text-white">
-			<div className="flex w-[19%] justify-between text-lg">
+		<header className="flex w-full justify-between bg-slate-900 py-5 text-white flex-wrap">
+			<div className="flex w-[19%] justify-between text-lg ml-10 my-3">
 				<Link
 					to="/"
-					className="flex items-center transition duration-300 ease-in-out hover:text-slate-300"
+					className="flex items-center transition duration-300 ease-in-out hover:text-slate-300 mr-5"
 				>
-					<h1>Pet Solidário</h1>
+					<img src={logo} alt="Pet solidario" className="w-20 max-w-xs mr-15" />
 				</Link>
 				<Link className="flex items-center transition duration-300 ease-in-out hover:text-slate-300">
 					<h1>Pets para adoção</h1>
 				</Link>
 			</div>
 
-			<div className="flex w-[15%] justify-between font-bold text-slate-900">
-				<Link className="rounded-xl bg-button-yellow px-5 py-2 transition duration-500 ease-in-out hover:bg-yellow-600">
+			<div className="flex justify-between font-bold text-slate-900 items-center gap-x-3 min-w-10 mx-10">
+				<Link className="rounded-xl bg-button-yellow px-5 py-2 transition duration-500 ease-in-out hover:bg-yellow-600 max-h-10">
 					Login
 				</Link>
 
-				<Link className="rounded-xl bg-button-yellow px-5 py-2 transition duration-500 ease-in-out hover:bg-yellow-600">
+				<Link className="rounded-xl bg-button-yellow px-5 py-2 transition duration-500 ease-in-out hover:bg-yellow-600 max-h-10">
 					Cadastra-se
 				</Link>
 			</div>
