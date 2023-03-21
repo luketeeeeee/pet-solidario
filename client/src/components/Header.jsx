58b1002a0ade63559ed7b55436e721d1b83afec0
Lucas Henrique <henrique.lucas521@gmail.com>
2023-03-21 14:20:24 -0300

    Foi feita a alteração do favicon do site e corrigi os espaçamentos do header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,20 +3,21 @@ import logo from '../assets/logo.png';
 
 export function Header() {
 	return (
-		<header className="flex w-full flex-wrap justify-between bg-slate-900 py-5 text-white">
-			<div className="my-3 ml-10 flex w-[19%] justify-between text-lg">
+		<header className="flex w-full flex-wrap justify-between bg-slate-900 px-16 py-5 text-white">
+			<div className="flex w-[15%] justify-between text-lg">
 				<Link
+					title="Logo do Pet Solidário"
 					to="/"
-					className="mr-5 flex items-center transition duration-300 ease-in-out hover:text-slate-300"
+					className="flex items-center transition duration-300 ease-in-out hover:text-slate-300"
 				>
-					<img src={logo} alt="Pet solidario" className="mr-15 w-20 max-w-xs" />
+					<img src={logo} alt="Pet solidario" className="w-16 max-w-xs" />
 				</Link>
 				<Link className="flex items-center transition duration-300 ease-in-out hover:text-slate-300">
 					<h1>Pets para adoção</h1>
 				</Link>
 			</div>
 
-			<div className="min-w-10 mx-10 flex items-center justify-between gap-x-3 font-bold text-slate-900">
+			<div className="min-w-10 flex items-center justify-between gap-x-3 font-bold text-slate-900">
 				<Link className="max-h-10 rounded-xl bg-button-yellow px-5 py-2 transition duration-500 ease-in-out hover:bg-yellow-600">
 					Login
 				</Link>
