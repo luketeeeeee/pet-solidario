4dd1772a8f84746e720f6c1e572265aa3a82b250
Lucas Henrique <henrique.lucas521@gmail.com>
2023-06-27 22:11:48 -0300

    adiciona funcionalidade de envio de email de um usuário para o outro

--- server/routes/passwordReset.js
+++ server/routes/passwordReset.js
@@ -1,16 +1,16 @@
-import UserSchema from '../mongodb/models/user.js';
-import TokenSchema from '../mongodb/models/token.js';
-import sendEmail from '../utils/sendEmail.js';
-import validateNewPassword from '../utils/validateNewPassword.js';
+import UserSchema from "../mongodb/models/user.js";
+import TokenSchema from "../mongodb/models/token.js";
+import { sendEmail } from "../utils/sendEmail.js";
+import validateNewPassword from "../utils/validateNewPassword.js";
 
-import crypto from 'crypto';
-import bcrypt from 'bcrypt';
-import Joi from 'joi';
-import express from 'express';
+import crypto from "crypto";
+import bcrypt from "bcrypt";
+import Joi from "joi";
+import express from "express";
 
 const router = express.Router();
 
-router.route('/').post(async (req, res) => {
+router.route("/").post(async (req, res) => {
     try {
         const { email } = req.body;
         const schema = Joi.object({ email: Joi.string().email().required() });
@@ -19,7 +19,7 @@ router.route('/').post(async (req, res) => {
         if (error) {
             console.log(error.details[0].message);
             return res.status(400).json({
-                message: 'Insira um email válido!',
+                message: "Insira um email válido!",
                 success: false,
             });
         }
@@ -28,7 +28,7 @@ router.route('/').post(async (req, res) => {
         if (!user) {
             return res.status(200).json({
                 message:
-                    'Se o email inserido estiver cadastrado na plataforma você receberá um email com instruções para trocar a senha!\n Não se esqueça de verificar sua caixa de spam!',
+                    "Se o email inserido estiver cadastrado na plataforma você receberá um email com instruções para trocar a senha!\n Não se esqueça de verificar sua caixa de spam!",
                 success: false,
             });
         }
@@ -36,33 +36,33 @@ router.route('/').post(async (req, res) => {
         let token = await TokenSchema.findOne({ userId: user._id });
         if (token) {
             return res.status(400).json({
-                message: 'Um token de recuperação já está ativo para este usuário!',
+                message: "Um token de recuperação já está ativo para este usuário!",
             });
         } else {
             token = await TokenSchema.create({
                 userId: user._id,
-                token: crypto.randomBytes(32).toString('hex'),
+                token: crypto.randomBytes(32).toString("hex"),
             });
         }
 
         const link = `http://localhost:5173/mudar-senha/${user._id}/${token.token}`;
         await sendEmail(
             user.email,
-            'Mude sua senha',
+            "Mude sua senha",
             `Olá, o link a seguir vai direcionar você para uma página em que será possível alterar sua senha\n ${link}`
         );
 
         res.status(200).json({
             message:
-                'Se o email inserido estiver cadastrado na plataforma você receberá um email com instruções para trocar a senha!\n Não se esqueça de verificar sua caixa de spam!',
+                "Se o email inserido estiver cadastrado na plataforma você receberá um email com instruções para trocar a senha!\n Não se esqueça de verificar sua caixa de spam!",
         });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: 'something is wrong, i can feel it' });
+        res.status(500).json({ message: "something is wrong, i can feel it" });
     }
 });
 
-router.route('/:userIdParam/:tokenParam').post(async (req, res) => {
+router.route("/:userIdParam/:tokenParam").post(async (req, res) => {
     try {
         const { userIdParam, tokenParam } = req.params;
         const { newPassword, confirmNewPassword } = req.body;
@@ -70,7 +70,7 @@ router.route('/:userIdParam/:tokenParam').post(async (req, res) => {
         const user = await UserSchema.findById(userIdParam);
         if (!user)
             return res.status(400).json({
-                message: 'O link é inválido ou expirou',
+                message: "O link é inválido ou expirou",
                 success: false,
             });
 
@@ -80,39 +80,39 @@ router.route('/:userIdParam/:tokenParam').post(async (req, res) => {
         });
         if (!token)
             return res.status(400).json({
-                message: 'O link é inválido ou expirou',
+                message: "O link é inválido ou expirou",
                 success: false,
             });
 
         if (newPassword !== confirmNewPassword) {
             return res.status(400).json({
-                message: 'As senhas não coincidem!',
+                message: "As senhas não coincidem!",
                 success: false,
             });
         }
 
         const { error } = validateNewPassword(req.body);
         if (error) {
-            if (error.details[0].type === 'passwordComplexity.numeric') {
+            if (error.details[0].type === "passwordComplexity.numeric") {
                 return res.status(400).json({
-                    message: 'A senha deve conter pelo menos um número!',
+                    message: "A senha deve conter pelo menos um número!",
                     success: false,
                 });
             }
 
             if (
-                error.details[0].type === 'passwordComplexity.uppercase' ||
-                error.details[0].type === 'passwordComplexity.lowercase'
+                error.details[0].type === "passwordComplexity.uppercase" ||
+                error.details[0].type === "passwordComplexity.lowercase"
             ) {
                 return res.status(400).json({
-                    message: 'A senha deve conter caracteres maiúsculos e minúsculos!',
+                    message: "A senha deve conter caracteres maiúsculos e minúsculos!",
                     success: false,
                 });
             }
 
-            if (error.details[0].type === 'passwordComplexity.symbol') {
+            if (error.details[0].type === "passwordComplexity.symbol") {
                 return res.status(400).json({
-                    message: 'A senha deve conter pelo menos um caracter especial!',
+                    message: "A senha deve conter pelo menos um caracter especial!",
                     success: false,
                 });
             }
@@ -125,10 +125,10 @@ router.route('/:userIdParam/:tokenParam').post(async (req, res) => {
         await user.save();
         await token.deleteOne();
 
-        res.status(200).json({ message: 'A senha foi alterada com sucesso!' });
+        res.status(200).json({ message: "A senha foi alterada com sucesso!" });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: 'something is wrong, i can feel it' });
+        res.status(500).json({ message: "something is wrong, i can feel it" });
     }
 });
 
