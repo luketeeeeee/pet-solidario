b51bdbc28b1ac717eb435da2df440a2417097d0f
Lucas Henrique <henrique.lucas521@gmail.com>
2023-07-15 16:19:41 -0300

    corrige responsividade das páginas de sobre nós e da home

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -5,20 +5,22 @@ export function AboutUs() {
 	return (
 		<>
 			<Header />
-			<main className="flex h-[85%] items-center justify-center bg-home-img bg-cover bg-no-repeat xl:h-[88%]">
-				<div className="flex h-[45%] w-[38%] flex-col items-center justify-between bg-black bg-opacity-60 p-6 text-center font-medium text-white">
-					<h1 className="w-full text-[2rem] font-medium text-button-yellow sm:text-[2.3rem]">
-						Sobre nós
-					</h1>
-					<p className="xl:w-3/3 w-full text-xl md:text-2xl">
-						Não somos uma associação nem temos instalações de canil ou gatil:
-						através dos anúncios aqui colocados ajudamos animais que estejam
-						para adoção a chegarem a quem os procure, criando assim
-						oportunidades de integrar animais em risco.
-					</p>
-					<p className="w-full text-xl md:text-2xl">
-						Juntos somos mais fortes!
-					</p>
+			<main className="flex h-[90%] items-center bg-home-img bg-cover bg-no-repeat xl:h-[88%] xl:justify-center">
+				<div className="mx-auto flex h-full w-full flex-col justify-center gap-7 text-center text-white md:h-[500px] lg:h-[500px] lg:w-3/5 xl:mx-0 xl:text-center 2xl:w-5/12">
+					<div className="flex h-[60%] w-full flex-col justify-between bg-black bg-opacity-60 p-6 xs:h-[55%] sm:h-[40%] md:h-[80%] md:py-14 lg:h-[90%] lg:items-center">
+						<h1 className="w-full text-[2rem] font-medium text-button-yellow sm:text-[2.3rem] md:text-3xl">
+							Sobre nós
+						</h1>
+						<p className="w-full text-xl font-medium md:text-2xl lg:w-[90%]">
+							Não somos uma associação nem temos instalações de canil ou gatil:
+							através dos anúncios do site ajudamos animais que estejam para
+							adoção a chegarem a quem os procure, criando assim oportunidades
+							de integrar animais em risco.
+						</p>
+						<p className="w-full text-xl font-medium md:text-2xl lg:w-[90%]">
+							Juntos somos mais fortes!
+						</p>
+					</div>
 				</div>
 			</main>
 		</>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,16 +5,16 @@ export function Home() {
 	return (
 		<>
 			<Header />
-			<main className="flex h-[90%] items-center bg-home-img bg-cover bg-no-repeat">
-				<div className="mx-auto flex h-full w-full flex-col justify-center gap-7 text-center text-white md:h-[600px] lg:h-[450px] xl:mx-0 xl:w-3/5 xl:text-center 2xl:w-5/12">
-					<div className="flex h-[60%] w-full flex-col justify-between bg-black bg-opacity-60 p-6 xs:h-[55%] sm:h-[40%] md:h-[70%]">
-						<h1 className="w-full text-[2rem] font-medium text-button-yellow sm:text-[2.3rem]">
+			<main className="flex h-[90%] items-center bg-home-img bg-cover bg-no-repeat xl:h-[88%] xl:justify-center">
+				<div className="mx-auto flex h-full w-full flex-col justify-center gap-7 text-center text-white md:h-[500px] lg:h-[500px] lg:w-3/5 xl:mx-0 xl:text-center 2xl:w-5/12">
+					<div className="flex h-[60%] w-full flex-col justify-between bg-black bg-opacity-60 p-6 xs:h-[55%] sm:h-[40%] md:h-[80%] md:py-14 lg:h-[90%] lg:items-center">
+						<h1 className="w-full text-[2rem] font-medium text-button-yellow sm:text-[2.3rem] md:text-3xl">
 							Conheça o Pet Solidário!
 						</h1>
-						<p className="xl:w-3/3 w-full text-xl font-medium md:text-2xl">
+						<p className="xl:w-3/3 w-full text-xl font-medium md:text-2xl lg:w-[60%]">
 							A adoção é um ato de amor e responsabilidade.
 						</p>
-						<p className="w-full text-xl font-medium md:text-2xl">
+						<p className="w-full text-xl font-medium md:text-2xl lg:w-[80%]">
 							O Pet Solidário é um site que apresenta uma maneira simples de se
 							adotar ou anunciar para adoção um animal que tenha sido encontrado
 							ou que esteja em risco de abandono.
